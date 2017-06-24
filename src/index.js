e3921c4bced946820103a12b2df9b5641ecf8452
Laci Szucs <ifrozen@gmail.com>
2017-06-24 15:34:32 +0200

    refactor(router): use <Switch /> in order to avoid double rendering

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import { Provider } from 'react-redux'
 import store from './store';
 
 import { ConnectedRouter } from 'react-router-redux';
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import createBrowserHistory from 'history/createBrowserHistory'
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(<Provider store={store}>
   <ConnectedRouter history={history}>
-    <Route exact path="/">
-      <App dataNamespace='badge-system' />
-    </Route>
+    <Switch>
+      <Route path='/react-riot'
+             render={() => <App dataNamespace='react-riot' />} />
+      <Route path='/'
+             render={() => <App dataNamespace='badge-system' />} />
+    </Switch>
   </ConnectedRouter>
 </Provider>, document.getElementById('root'));
 
