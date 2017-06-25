aebc2437ecef8b2fe133d782b984714a6b809366
Laci Szucs <ifrozen@gmail.com>
2017-06-25 12:48:37 +0200

    feat(Skills): layout for the tag and star icon and numbers in the panel header

--- src/containers/Skills.js
+++ src/containers/Skills.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import { Button, Glyphicon, Collapse } from 'react-bootstrap';
+import './Skills.css';
 
-const outlineStyle = {
-  marginLeft: 5,
-  backgroundColor: 'transparent',
-  borderColor: 'rgba(0,0,0, 0.2)',
-  transition: 'all .5s'
-};
 
-const accordionHeader = ({category, handleToggle, isOpen}) => <span>
+
+const skillCategoryPanelHeader = ({
+  category,
+  handleToggle,
+  isOpen,
+  earnedCategoryBadgeCount=0,
+  earnedCategoryStarCount=0
+}) => <span className='skillPanelHeader'>
   <span onClick={() => handleToggle(category.key)}>
     <Glyphicon glyph={`triangle-${isOpen ? 'top' : 'bottom'}`} style={{marginRight: 5}} />
     {category.name}
+    <Glyphicon className='skillPanelHeaderGlyph' glyph='tag' /> {earnedCategoryBadgeCount}
+    <Glyphicon className='skillPanelHeaderGlyph' glyph='star' /> {earnedCategoryStarCount}
   </span>
-  <Button type='button' bsSize={'sm'} style={outlineStyle}>
-    <Glyphicon glyph='plus' /> Add
-  </Button>
-  <Button type='button' bsSize={'sm'} style={outlineStyle}>
-    <Glyphicon glyph='tag' /> See all
-  </Button>
+
+  <span>
+    <Button type='button' bsSize={'sm'} className='skillPanelHeaderButton'>
+      <Glyphicon glyph='plus' /> Add
+    </Button>
+    <Button type='button' bsSize={'sm'} className='skillPanelHeaderButton'>
+      <Glyphicon glyph='tag' /> See all
+    </Button>
+  </span>
 </span>;
 
 export default class Skills extends Component {
@@ -41,7 +48,7 @@ export default class Skills extends Component {
         const isOpen =this.state.panelOpen[category.key];
         return <div className="panel panel-default">
           <div className="panel-heading" style={{backgroundColor: category.color}}>
-            {accordionHeader({category, handleToggle: this.handleToggle, isOpen })}
+            {skillCategoryPanelHeader({category, handleToggle: this.handleToggle, isOpen })}
           </div>
           <Collapse in={isOpen}>
             <div className="panel-body">
