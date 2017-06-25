9c3719c611bd9900cd4b43495b266eb1c9e2d875
Simon Márton <simon.marton@gmail.com>
2017-06-25 15:02:47 +0200

    feat(Slide): Basic slider created.

---
+++ src/components/Slider/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RCSlider, { Handle }  from 'rc-slider';
+import Tooltip from 'rc-tooltip';
+import 'rc-slider/assets/index.css';
+
+import './style.css';
+
+import Face from './Face';
+
+export default class Slider extends React.Component {
+  static propTypes = {
+    levelCount: PropTypes.number.isRequired,
+    currentLevel: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired,
+    active: PropTypes.bool,
+
+    onChange: PropTypes.func,
+    onDelete: PropTypes.func
+  };
+
+  static defaultProps = {
+    active: false
+  };
+
+  handle = ({ value, dragging, index, ...restProps }) => (
+    <Tooltip
+      overlay={() => <Face color={this.props.color} collapsed={!this.props.active} />}
+      placement='bottom'
+      key={index}
+      visible
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  )
+
+  render() {
+    const wrapperStyle = { width: 400, margin: 50 };
+
+    return (
+      <div className='Slider'>
+        <RCSlider min={0} max={this.props.levelCount} defaultValue={this.props.currentLevel} handle={this.handle} />
+      </div>
+    );
+
+
+    return (
+      <div style={{ backgroundColor: 'red'}}>
+      <RCSlider />
+      </div>
+    );
+  }
+}
